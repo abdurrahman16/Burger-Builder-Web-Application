1c24b4e8bb07e57c41516eb0c319ba38ccc6ce9d
ラハマン <rahman.just16@gmail.com>
2026-01-24 11:01:18 +0900

    Bruger Materials Added

--- src/Components/BurgerBuilder/BurgerBuilder.js
+++ src/Components/BurgerBuilder/BurgerBuilder.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
+import Burger from './Burger/Burger.js'
 
 export class BurgerBuilder extends Component {
   render() {
     return (
-      <div>BurgerBuilder</div>
+      <div>
+        <Burger />
+      </div>
     )
   }
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,33 @@
 import React from 'react'
+import Logo from '../../assets/logo.png'
+import './Header.css';
+import {
+  Navbar,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
 
-const Header = () =>{
-  return (
-    <div>
-        <p> Header</p>
-    </div>
-  )
+const Header = () => {
+    return (
+        <div className="Navigation">
+            <Navbar style={{
+                backgroundColor: "#D70F64",
+                height: "70px",
+            }}>
+                <NavbarBrand href="/" className="mr-auto ml-md-5 Brand">
+                    <img src={Logo} alt="Logo" width="80px" />
+                </NavbarBrand>
+                <Nav className="mr-md-5">
+                    <NavItem>
+                        <NavLink href="#" className="NavLink" style={{color:'white'}}>Something</NavLink>
+                    </NavItem>
+                </Nav>
+            </Navbar>
+        </div>
+    )
 }
+
 
 export default Header;
